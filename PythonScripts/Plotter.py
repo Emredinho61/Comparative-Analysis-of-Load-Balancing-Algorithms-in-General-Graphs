c77f4fe1a6ac784c320bdeb8c0dc7830190c7c3f
Emre Bayazitoglu <emre-samet61@hotmail.de>
2024-07-28 13:09:44 +0200

    Rearranged Images and new Plotter with new plots

--- PythonScripts/Plotter.py
+++ PythonScripts/Plotter.py
@@ -13,28 +13,24 @@ class Plotter:
         rounds = []
         MSEs = []
         try:
-            file = open(filepath, 'rt')
-            for one_line in file:
-                regex = r"(Cycle No.: )([\d]*)|(MSE: )([\d|.|E\-\d]*)|(Config: )([\w| ]*)"
-                matches = re.finditer(regex, one_line, re.MULTILINE)
-                for match in matches:
-                    # Round is second Element of Regex group
-                    if match.groups()[1] is not None:
-                        rounds.append(match.groups()[1])
-                    # MSE is 4th of Regex group
-                    if match.groups()[3] is not None:
-                        MSEs.append(match.groups()[3])
-                    if match.groups()[5] is not None:
-                        network_information = match.groups()[5].split(" ")
-                        self.networksize = network_information[0]
-                        self.networktype = " ".join(network_information[1:])
-            file.close()
+            with open(filepath, 'rt') as file:
+                for one_line in file:
+                    regex = r"(Cycle No.: )([\d]*)|(MSE: )([\d|.|E\-\d]*)|(Config: )([\w| ]*)"
+                    matches = re.finditer(regex, one_line, re.MULTILINE)
+                    for match in matches:
+                        if match.groups()[1] is not None:
+                            rounds.append(match.groups()[1])
+                        if match.groups()[3] is not None:
+                            MSEs.append(match.groups()[3])
+                        if match.groups()[5] is not None:
+                            network_information = match.groups()[5].split(" ")
+                            self.networksize = network_information[0]
+                            self.networktype = " ".join(network_information[1:])
             return [rounds, MSEs]
         except Exception as e:
-            print(f"An exception occured while opening the file {e}")
+            print(f"An exception occurred while opening the file {e}")
 
     def abbreviate_networktype(self):
-        print(self.networktype)
         abbreviaion = [oneWord[0] for oneWord in self.networktype.split(" ")]
         abbreviaion_string = "".join(abbreviaion)
         return abbreviaion_string
@@ -44,30 +40,34 @@ class Plotter:
         MSE_Rounds_PPS = self.MSE_per_Round(filepath_PPS)
         initial_MSE = float(MSE_Rounds_PPS[1][0])
         MSE_Rounds_DAB[1].insert(0, initial_MSE)
-        print(MSE_Rounds_DAB)
-        DAB_Plot = plt.plot([oneRound for oneRound in range(0, len(MSE_Rounds_DAB[0]) + 1, 1)],
-                            [float(oneMSE) for oneMSE in MSE_Rounds_DAB[1][0:50:1]], label="DAB")
-        plt.setp(DAB_Plot, color='r', linewidth=linewidth)
-        PPS_PLot = plt.plot([oneRound for oneRound in range(0, len(MSE_Rounds_PPS[0]) + 1, 1)],
-                            [float(oneMSE) for oneMSE in MSE_Rounds_PPS[1][0:50:1]], label="PPS")
-        plt.setp(PPS_PLot, color='g', linewidth=linewidth)
+
+        plt.figure(figsize=(10, 6))
+        rounds_range_DAB = range(0, len(MSE_Rounds_DAB[0]) + 1, 1)
+        rounds_range_PPS = range(0, len(MSE_Rounds_PPS[0]) + 1, 1)
+
+        DAB_Plot = plt.plot(rounds_range_DAB,
+                            [float(oneMSE) for oneMSE in MSE_Rounds_DAB[1][0:50:1]],
+                            label="DAB", color='r', linewidth=linewidth, marker='o', markersize=4)
+
+        PPS_Plot = plt.plot(rounds_range_PPS,
+                            [float(oneMSE) for oneMSE in MSE_Rounds_PPS[1][0:50:1]],
+                            label="PPS", color='g', linewidth=linewidth, marker='x', markersize=4)
+
         plt.yscale('log')
-        plt.xlabel("Rounds")
-        plt.ylabel("Mean Squared Error")
-        plt.title(f"Mean Squared Error per Round - Networksize {self.networksize} {self.networktype}")
-        leg = plt.legend(loc='upper center')
-        # plt.show()
+        plt.xlabel("Rounds", fontsize=12)
+        plt.ylabel("Mean Squared Error", fontsize=12)
+        plt.title(f"Network Size {self.networksize} {self.networktype}", fontsize=14)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.legend(loc='upper right', fontsize=10)
+        plt.tight_layout()
+
         abbreviation = self.abbreviate_networktype()
-        plt.savefig(f"Plots/DAB_vs_PPS_{abbreviation}_r50_n{self.networksize}", transparent=None, dpi=700, format=None,
-                    metadata=None, bbox_inches=None, pad_inches=0.1,
-                    facecolor='auto', edgecolor='auto', backend=None,
-                    )
+        plt.savefig(f"Plots/DAB_vs_PPS_{abbreviation}_r50_n{self.networksize}.png",
+                    transparent=False, dpi=700, bbox_inches='tight', pad_inches=0.1)
 
 
 if __name__ == "__main__":
-    file1 = "../simulationResults/terminalOutput_DAB_10000.txt"
-    file2 = "../simulationResults/terminalOutput_PPS_10000.txt"
-    plotter_instance = Plotter(
-        [file1, file2])
-
+    file1 = "./Plots/StarGraph/terminalOutput_DAB_10000.txt"
+    file2 = "./Plots/StarGraph/terminalOutput_PPS_10000.txt"
+    plotter_instance = Plotter([file1, file2])
     plotter_instance.plot_MSE(file1, file2, linewidth=0.5)
